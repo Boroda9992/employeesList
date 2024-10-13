52a44b8cc38dad8e31c97dd9f8eb2191e427f856
Boroda9992 <fedko.ak@gmail.com>
2024-10-13 21:53:51 +0300

    initial commit

---
+++ src/main/java/pro/sky/employeesList/EmployeesListApplication.java
@@ -0,0 +1,15 @@
+package pro.sky.employeesList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesListApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/pro/sky/employeesList/EmployeesListApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.employeesList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
